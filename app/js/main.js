98a19d2aa2a623ee69f7db008a00ee7c414d1ca2
Javier Burón <javierburon@colaborativa.eu>
2014-02-07 20:30:52 +0100

    finishing landing page

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,8 @@ $(function(){
 	  delay:9000,
 	  backgrounds:[
 	    { src:'/img/background_1.jpg', fade:3000 },
-	    { src:'/img/background_2.jpg', fade:3000 }
+	    { src:'/img/background_2.jpg', fade:3000 },
+	    { src:'/img/background_3.jpg', fade:3000 }
 	  ]
 	});
 });
