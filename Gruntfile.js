41300955f4869fb9fad6b6041969d83b46ddfe05
Javier Burón <javierburon@colaborativa.eu>
2014-02-08 15:17:10 +0100

    modify

--- Gruntfile.js
+++ Gruntfile.js
@@ -295,7 +295,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'master',
+          remote: 'git://github.com/FabLabLimerick/fablablimerick.github.io.git',
           branch: 'master',
           commit: true,
           push: true
