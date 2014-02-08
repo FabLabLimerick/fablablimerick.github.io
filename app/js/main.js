a7bc0c9ccaf0c03a691bed76735a5042b62e0e8e
Javier Burón <javierburon@colaborativa.eu>
2014-02-08 18:51:44 +0100

    css for iphone

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,11 @@
 $(function(){
 	$.vegas('slideshow', {
 	  delay:9000,
+	  preload:false,
 	  backgrounds:[
-	    { src:'/img/background_1.jpg', fade:3000 },
-	    { src:'/img/background_2.jpg', fade:3000 },
-	    { src:'/img/background_3.jpg', fade:3000 }
+	    { src:'/img/background_1.jpg', fade:3000, loading:false },
+	    { src:'/img/background_2.jpg', fade:3000, loading:false },
+	    { src:'/img/background_3.jpg', fade:3000, loading:false }
 	  ]
 	});
 });
