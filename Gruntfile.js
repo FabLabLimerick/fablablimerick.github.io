22772b2de2c13b56f5576fa3b56340b2a8ee822c
Javier Burón <javierburon@colaborativa.eu>
2014-02-10 14:41:52 +0100

    solve problem with socialmeta images

--- Gruntfile.js
+++ Gruntfile.js
@@ -261,7 +261,8 @@ module.exports = function (grunt) {
             // Explicitly add any files your site needs for distribution here.
             '_bower_components/jquery/jquery.js',
             'favicon.ico',
-            'apple-touch*.png'
+            'logo-thumbnail.png',
+            'thumbnail.jpg'
           ],
           dest: '<%= yeoman.dist %>'
         }]
