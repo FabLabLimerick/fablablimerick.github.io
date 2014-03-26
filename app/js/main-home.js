f92ffc5821b456d1f8bc964a5cfadac41eb8fb4b
magda <magdasanchez@colaborativa.eu>
2014-03-26 17:51:22 +0100

    Adding javascript for carousel and menucollapse for mobile

--- app/js/main-home.js
+++ app/js/main-home.js
@@ -1,1 +1,14 @@
 
+$("div.menucollapse").click(function(){
+	if( $("#botones").hasClass("collapse") == true){
+		$("#botones").removeClass("collapse");
+	}else{
+		$("#botones").addClass("collapse");
+	}	
+});
+
+
+$("div#botones").click(function(){
+	$("#botones").removeClass("collapse").addClass("collapse");	
+});
+
