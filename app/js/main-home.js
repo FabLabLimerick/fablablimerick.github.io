b306c105b0807a0b0bd166cc73fc95ab898f8d75
magda <magdasanchez@colaborativa.eu>
2015-02-24 18:53:46 +0100

    Fixing out list of next and old events in javascript

--- app/js/main-home.js
+++ app/js/main-home.js
@@ -11,3 +11,51 @@ $('div.menucollapse').click(function(){
 $('div#botones').click(function(){
 	$('#botones').removeClass('collapsing').addClass('collapsing');
 });
+
+/*
+	February- 2015
+	Improving list of events
+*/
+$( document ).ready(function() {
+	var num = $("#events .container .section-list").children("article").size();
+    // Hide past events on Home Page
+    $.each($("#events .container .section-list").children("article"),function(index,value){
+    	var currentDate = new Date("2015-04-06");
+    	var eventDate = new Date(this.getAttribute("date"));
+    	if(eventDate <  currentDate){
+    		$(this).next("hr").remove();
+    		$(this).remove();
+    		}
+    	// Case of 0 events on main, show "More events soon"
+		if ( index == (num-1)){
+	    	if($("#events .container .section-list").children('article').size() == 0){
+		    	$("#events .container .section-list").remove();
+		    	$("#events .container #nextEvents").text("MORE EVENTS COMING SOON");
+	    	}
+	    }
+    });
+    // Hide past events on Event Page
+    num = $(".body-events.current .section-list").children("article").size();
+    $.each($(".body-events.current .section-list").children("article"),function(index,value){
+		var currentDate = new Date();	
+		var eventDate = new Date(this.getAttribute("date"));
+		if(eventDate <  currentDate){ // It is a previous event
+			$(this).addClass("past");
+			if($(".body-events.previous .section-list hr").first().hasClass("gray") == false ){
+				$(".body-events.previous .section-list hr").first().addClass("gray");
+			}
+    		//$(".body-events.previous .section-list").html("");			
+    		$(".body-events.previous .section-list").prepend(this.outerHTML);
+    		$(".body-events.previous .section-list").prepend("<hr class='event'>");
+    		$(this).next("hr").remove();
+    		$(this).remove();
+    			
+    	}
+    	if ( index == (num-1)){
+    		console.log("events");
+			if($(".body-events.current .section-list").children('article').size() == 0){
+						$(".body-events.current .section-header").remove();
+			}
+    	}
+	}); // end each
+});
